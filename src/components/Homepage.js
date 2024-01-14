7ff0cbf3bbcd94a18fb9d301f0e681b6872b6586
deehas <sahbdulsalam@gmail.com>
2024-01-15 00:38:29 +0100

    update: filter todos

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,9 +2,22 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPenNib } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPlus,
+  faTasks,
+  faSmile,
+  faTrash,
+  faCalendar,
+  faClock,
+} from "@fortawesome/free-solid-svg-icons";
+import { ToastContainer, toast } from "react-toastify";
+
+// Internal imports
+import ToastConfig from "./utils/utils";
 
 function HomePage(props) {
+  const currentDate = new Date();
+  const currentTime = currentDate.getTime();
   const [homePageData, setHomePageData] = useState(null);
   const [personalTaskCount, setPersonalTaskCount] = useState(0);
   const [businessTaskCount, setBusinessTaskCount] = useState(0);
@@ -18,54 +31,74 @@ function HomePage(props) {
 
   useEffect(() => {
     const username = localStorage.getItem("userName");
+    const userId = localStorage.getItem("userID");
 
     setUserName(username);
+
+    // Load todos on page load
+    getTodos(userId);
   }, []);
 
-  function getData() {
+  function getTodos(userId) {
     axios({
       method: "GET",
-      url: "/home",
+      url: `/home/${userId}`,
       headers: {
         Authorization: "Bearer " + props.token,
       },
     })
       .then((response) => {
         const res = response.data;
-        res.access_token && props.setToken(res.access_token);
-        setHomePageData({
-          profile_name: res.name,
-          about_me: res.about,
-        });
+        const personalTaskCount = res.no_of_personal_tasks;
+        const businessTaskCount = res.no_of_business_tasks;
+        const otherTaskCount = res.no_of_other_tasks;
+        const categoryOneName = res.category_i_name;
+        const categoryTwoName = res.category_ii_name;
+        const categoryThreeName = res.category_iii_name;
+        const todos = res.todos;
+
+        setPersonalTaskCount(personalTaskCount);
+        setBusinessTaskCount(businessTaskCount);
+        setOtherTaskCount(otherTaskCount);
+        setCategoryOneName(categoryOneName);
+        setCategoryTwoName(categoryTwoName);
+        setCategoryThreeName(categoryThreeName);
+        setTodos(todos);
       })
       .catch((error) => {
         if (error.response) {
-          console.log(error.response);
-          console.log(error.response.status);
-          console.log(error.response.headers);
+          const data = error.response;
+          toast.error(data.data.msg, ToastConfig);
         }
       });
   }
 
+  function dateStrToTime(params) {
+    const actualDate = new Date(params);
+
+    return actualDate.getTime();
+  }
+
   return (
     <div className="main_content">
+      <ToastContainer />
       <div className="welcome">
         <button id="openbutton" className="openbtn">
           &#9776;
         </button>
         <span className="welcome-text">
-          Welcome<span className="user">{userName}</span>
-          <span className="exclamation-mark">!</span>
-          <i className="fa fa-smile-o" aria-hidden="true"></i>
+          Welcome <span className="user">{userName}</span>
+          <span className="exclamation-mark">! </span>
+          <FontAwesomeIcon icon={faSmile} />
         </span>
         {otherTaskCount > 0 ||
           businessTaskCount > 0 ||
           (personalTaskCount > 0 && (
             <div className="taskAdd">
               <a href="/tasks" className="btn btn-primary">
-                <i className="fa fa-plus icon"></i> <span class="Add">Add</span>
+                <FontAwesomeIcon icon={faPlus} /> <span class="Add">Add</span>
                 <span className="or">or</span>{" "}
-                <i className="fa fa-trash-o icon"></i>
+                <FontAwesomeIcon icon={faTrash} />
                 <span className="Delete">Delete Task(s)</span>
               </a>
             </div>
@@ -104,7 +137,7 @@ function HomePage(props) {
             type="radio"
             id="tab-1"
             name="tab-effect-1"
-            checked="checked"
+            defaultChecked="checked"
           />
           <span>All</span>
 
@@ -118,120 +151,82 @@ function HomePage(props) {
             <section id="tab-item-1">
               <ul className="taskList">
                 {todos &&
-                  todos.map((todo, index) => {
+                  todos.map((todo, index) => (
                     <li key={index} className="currentTaskItem">
-                      <i className="fa fa-tasks" aria-hidden="true"></i>
+                      <FontAwesomeIcon icon={faTasks} />
                       <span id="TaskDone" className="complete- taskDone">
                         {todo.title}
                       </span>
                       <span className="taskDone">at</span>
                       <strong className="taskDone">
-                        <i className="fa fa-clock-o"></i> {todo.time}
+                        <FontAwesomeIcon icon={faClock} /> {todo.time}
                       </strong>
                       <span className="taskDone">on</span>
+                        <FontAwesomeIcon icon={faCalendar} />
                       <strong className="taskDatee taskDone">
-                        <i className="fa fa-calendar"></i> {todo.date}
+                        {todo.date}
                       </strong>
                       <span className="categoryy-{ todo.category }">
                         {todo.category}
                       </span>
-                    </li>;
-                  })}
+                    </li>
+                  ))}
               </ul>
             </section>
             <section id="tab-item-2">
               <ul id="currentTasks" className="taskList">
                 {todos &&
-                  todos.map((todo, index) => {
-                    {
-                      /* {% if todo.date.strftime('%Y-%m-%d') > DateNow %}  */
-                    }
-                    <li className="currentTaskItem">
-                      <i className="fa fa-tasks" aria-hidden="true"></i>
-                      <span id="TaskDone" className="complete- taskDone">
-                        {todo.title}
-                      </span>
-                      <span className="taskDone">at</span>
-                      <strong className="taskDone">
-                        <i className="fa fa-clock-o"></i> {todo.time}
-                      </strong>
-                      <span className="taskDone">on</span>
-                      <strong className="taskDatee taskDone">
-                        <i className="fa fa-calendar"></i> {todo.date}
-                      </strong>
-                      <span className="categoryy-{{ todo.category }}">
-                        {todo.category}
-                      </span>
-                    </li>;
-
-                    {
-                      /* {% if todo.date.strftime('%Y-%m-%d') == DateNow and todo.time.strftime('%H:%M') >= TimeNow %} */
-                    }
-                    <li className="currentTaskItem">
-                      <i className="fa fa-tasks" aria-hidden="true"></i>
-                      <span id="TaskDone" className="complete- taskDone">
-                        {todo.title}
-                      </span>
-                      <span className="taskDone">at</span>
-                      <strong className="taskDone">
-                        <i className="fa fa-clock-o"></i> {todo.time}
-                      </strong>
-                      <span className="taskDone">on</span>
-                      <strong className="taskDatee taskDone">
-                        <i className="fa fa-calendar"></i> {todo.date}
-                      </strong>
-                      <span className="categoryy-{{ todo.category }}">
-                        {todo.category}
-                      </span>
-                    </li>;
-                  })}
+                  todos.map((todo, index) => (
+                    <div key={index}>
+                      {dateStrToTime(todo.date) > currentTime && (
+                        <li className="currentTaskItem">
+                          <FontAwesomeIcon icon={faTasks} />
+                          <span id="TaskDone" className="complete- taskDone">
+                            {todo.title}
+                          </span>
+                          <span className="taskDone">at</span>
+                          <strong className="taskDone">
+                            <FontAwesomeIcon icon={faClock} /> {todo.time}
+                          </strong>
+                          <span className="taskDone">on</span>
+                          <strong className="taskDatee taskDone">
+                            <FontAwesomeIcon icon={faCalendar} /> {todo.date}
+                          </strong>
+                          <span className="categoryy-{{ todo.category }}">
+                            {todo.category}
+                          </span>
+                        </li>
+                      )}
+                    </div>
+                  ))}
               </ul>
             </section>
             <section id="tab-item-3">
               <ul id="overdueTasks" className="taskList">
                 {todos &&
-                  todos.map((todo, index) => {
-                    {
-                      /* { if DateNow > todo.date.strftime('%Y-%m-%d') } */
-                    }
-                    <li className="overdueTaskItem">
-                      <i className="fa fa-tasks" aria-hidden="true"></i>
-                      <span id="TaskDone" className="complete- taskDone">
-                        {todo.title}
-                      </span>
-                      <span className="taskDone">at</span>
-                      <strong className="taskDone">
-                        <i className="fa fa-clock-o"></i> {todo.time}
-                      </strong>
-                      <span className="taskDone">on</span>
-                      <strong className="taskDatee taskDone">
-                        <i className="fa fa-calendar"></i> {todo.date}
-                      </strong>
-                      <span className="categoryy-{{ todo.category }}">
-                        {todo.category}
-                      </span>
-                    </li>;
-                    {
-                      /* { if DateNow == todo.date.strftime('%Y-%m-%d') and todo.time.strftime('%H:%M') < TimeNow } */
-                    }
-                    <li className="overdueTaskItem">
-                      <i className="fa fa-tasks" aria-hidden="true"></i>
-                      <span id="TaskDone" className="complete- taskDone">
-                        {todo.title}
-                      </span>
-                      <span className="taskDone">at</span>
-                      <strong className="taskDone">
-                        <i className="fa fa-clock-o"></i> {todo.time}
-                      </strong>
-                      <span className="taskDone">on</span>
-                      <strong className="taskDatee taskDone">
-                        <i className="fa fa-calendar"></i> {todo.date}
-                      </strong>
-                      <span className="categoryy-{{ todo.category }}">
-                        {todo.category}
-                      </span>
-                    </li>;
-                  })}
+                  todos.map((todo, index) => (
+                    <div key={index}>
+                      {currentTime > dateStrToTime(todo.date) && (
+                        <li className="overdueTaskItem">
+                          <FontAwesomeIcon icon={faTasks} />
+                          <span id="TaskDone" className="complete- taskDone">
+                            {todo.title}
+                          </span>
+                          <span className="taskDone">at</span>
+                          <strong className="taskDone">
+                            <FontAwesomeIcon icon={faClock} /> {todo.time}
+                          </strong>
+                          <span className="taskDone">on</span>
+                            <FontAwesomeIcon icon={faCalendar} />
+                          <strong className="taskDatee taskDone"> {todo.date}
+                          </strong>
+                          <span className="categoryy-{{ todo.category }}">
+                            {todo.category}
+                          </span>
+                        </li>
+                      )}
+                    </div>
+                  ))}
               </ul>
             </section>
 
@@ -244,7 +239,7 @@ function HomePage(props) {
                   </div>
                   <div className="Addtask">
                     <a href="/tasks" className="btn btn-primary">
-                      <i className="fa fa-plus icon"></i>
+                      <FontAwesomeIcon icon={faPlus} />
                       <span className="Add">Add Task(s) </span>
                     </a>
                   </div>
@@ -257,7 +252,7 @@ function HomePage(props) {
           (personalTaskCount > 0 && (
             <div className="Addtask">
               <a href="/tasks" className="btn btn-primary">
-                <i className="fa fa-plus icon"></i>
+                <FontAwesomeIcon icon={faPlus} />
               </a>
             </div>
           ))}
